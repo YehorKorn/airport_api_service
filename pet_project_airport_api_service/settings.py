db1a76b35f7e9b94c23c742b14c03dd4692a09bf
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-04 21:31:42 +0300

    Changed from ' to "

--- pet_project_airport_api_service/settings.py
+++ pet_project_airport_api_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sy^%m4z0bfxv#qbw3!@3ti8plns@kd-%py*f7efdn8c33j$3%e'
+SECRET_KEY = "django-insecure-sy^%m4z0bfxv#qbw3!@3ti8plns@kd-%py*f7efdn8c33j$3%e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
