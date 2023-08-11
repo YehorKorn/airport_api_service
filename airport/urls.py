0b132075b794e92dbffe2400dc5fd1c1f0521b56
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-11 16:04:13 +0300

    Registered router for crews

--- airport/urls.py
+++ airport/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from rest_framework import routers
 
 from airport.views import (
-    FlightViewSet,
     AirplaneTypeViewSet,
     AirplaneViewSet,
     AirportViewSet,
+    FlightViewSet,
     RouteViewSet,
     CrewViewSet,
     OrderViewSet,
@@ -13,6 +13,7 @@ from airport.views import (
 
 router = routers.DefaultRouter()
 router.register("flights", FlightViewSet)
+router.register("crews", CrewViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
