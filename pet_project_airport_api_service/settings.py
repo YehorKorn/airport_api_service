2d0083e9a2c5088f0c4d5f5bcba7fbe70956da2d
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-11 20:29:35 +0300

    Added dotenv and moved SECRET_KEY to .env

--- pet_project_airport_api_service/settings.py
+++ pet_project_airport_api_service/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sy^%m4z0bfxv#qbw3!@3ti8plns@kd-%py*f7efdn8c33j$3%e"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
