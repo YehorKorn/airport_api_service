9b7ae2bbbf14aa8066ad556733c451e0f8b7f3b5
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-11 18:54:38 +0300

    Deleted import for PageNumberPagination

--- airport/views.py
+++ airport/views.py
@@ -5,7 +5,6 @@ from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import mixins, status
 from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
