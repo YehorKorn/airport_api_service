25d9ce6ac18de7c765a98dcb0307a0d7a35dbe07
Yehor Kornev <dmekkindom00@gmail.com>
2023-08-11 16:26:17 +0300

    Added view for AirplaneType

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.db.models import F, Count
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
+from airport.models import (
+    AirplaneType,
+    Airplane,
+    Airport,
+    Crew,
+    Route,
+    Flight,
+    Order,
+)
+from airport.permissions import IsAdminOrIfAuthenticatedReadOnly
+
+from airport.serializers import (
+    AirplaneSerializer,
+    CrewSerializer,
+    OrderSerializer,
+    OrderListSerializer,
+    FlightSerializer,
+    AirplaneTypeSerializer,
+    AirportSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    FlightListSerializer,
+    AirplaneListSerializer,
+    AirplaneImageSerializer,
+    FlightDetailSerializer,
+)
+
+
+class AirplaneTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
